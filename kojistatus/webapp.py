e281fb9d6f9ad8dec5ad46aa6a157b770ad00400
Miro Hrončok <miro@hroncok.cz>
2017-01-21 17:28:00 +0100

    Don't use lists where not necessary

--- kojistatus/webapp.py
+++ kojistatus/webapp.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/<username>/')
 def main(username):
     try:
-        lines = ['{} {}'.format(*s) for s in status(username)]
+        lines = ('{} {}'.format(*s) for s in status(username))
     except HTTPError:
         # This is most likely due to not existing username, so return 404
         # But it might also be a Koji problem, because it just gives 500
