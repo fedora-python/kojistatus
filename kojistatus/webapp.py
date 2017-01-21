26cd71919a922687bd5f6696f550808d1c411e28
Miro Hrončok <miro@hroncok.cz>
2017-01-21 17:12:31 +0100

    Return 404 on Koji's 500

--- kojistatus/webapp.py
+++ kojistatus/webapp.py
@@ -1,4 +1,6 @@
 from flask import Flask, Response
+from flask.helpers import NotFound
+from requests.exceptions import HTTPError
 
 from .status import status
 
@@ -10,7 +12,8 @@ app = Flask(__name__)
 def main(username):
     try:
         lines = ['{} {}'.format(*s) for s in status(username)]
-    except:
-        lines = []
-        ...  # TODO return 500-ish error
+    except HTTPError:
+        # This is most likely due to not existing username, so return 404
+        # But it might also be a Koji problem, because it just gives 500
+        raise NotFound()
     return Response('\n'.join(lines), mimetype='text/plain')
