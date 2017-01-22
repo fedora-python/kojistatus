29eb3194920dc91a99ca4911f82f8935f9f82d79
Miro Hrončok <miro@hroncok.cz>
2017-01-22 18:44:56 +0100

    Add an example MicroPython client

--- README.rst
+++ README.rst
@@ -4,4 +4,6 @@ kojistatus
 A little Python tool to fetch the last statuses of Fedora Koji builds of
 a given user. Created mostly to provide simple text API for MicroPython.
 
+See an example MicroPython client in ``examples`` directory.
+
 Licensed under the MIT license, see LICENSE.
---
+++ example/main.py
@@ -0,0 +1,81 @@
+import utime as time
+import usocket as socket
+
+
+PIN_NUM = 2
+NUM_LEDS = 8
+SLEEP_TIME = 30
+HOST = 'kojistatus-hroncok.rhcloud.com'
+USERNAME = 'churchyard'
+
+
+try:
+    from machine import Pin
+    from neopixel import NeoPixel
+    pin = Pin(PIN_NUM, Pin.OUT)
+    np = NeoPixel(pin, NUM_LEDS)
+except ImportError:
+    # a real computer, no HW access
+
+    class FakeNeoPixel(list):
+        def __init__(self):
+            super()
+            for led in range(NUM_LEDS):
+                self.append((0, 0, 0))
+
+        def write(self):
+            print(' '.join('{}'.format(c) for c in self))
+
+    np = FakeNeoPixel()
+
+
+PATH = USERNAME + '/'
+
+
+def download_status(addr):
+    s = socket.socket()
+    s.connect(addr)
+    s.send(bytes(
+        'GET /{} HTTP/1.0\r\nHost: {}\r\n\r\n'.format(PATH, HOST), 'utf8'))
+    data = ''
+    while True:
+        chunk = s.recv(100)
+        if chunk:
+            data += chunk.decode('utf-8')
+        else:
+            break
+    s.close()
+
+    head, body = data.split('\r\n\r\n', 2)
+
+    yield from body.split('\n')
+
+
+COLORS = {
+    'free': (0, 0, 255),
+    'open': (255, 255, 0),
+    'failed': (255, 0, 0),
+    'canceled': (127, 0, 0),
+    'closed': (0, 255, 0),
+}
+
+
+def last_colors(num):
+    ai = socket.getaddrinfo(HOST, 80)
+    addr = ai[0][-1]
+    for idx, line in enumerate(download_status(addr)):
+        if idx == num:
+            break
+        _, status = line.split()
+        yield COLORS[status]
+
+
+time.sleep(20)  # Give it a time to connect
+while True:
+    try:
+        for led, color in enumerate(last_colors(NUM_LEDS)):
+            np[led] = color
+        np.write()
+    except:
+        pass
+    time.sleep(SLEEP_TIME)
