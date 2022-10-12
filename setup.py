c7c56b25212f5e1a71727133192674d4d56614b7
Lumir Balhar <lbalhar@redhat.com>
2022-10-12 15:10:40 +0200

    Drop support for Python 3.6

--- setup.py
+++ setup.py
@@ -35,7 +35,6 @@ setup(
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
